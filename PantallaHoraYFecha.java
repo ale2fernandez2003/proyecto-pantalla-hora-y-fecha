5083bf242559f6ba28611dd1aff3d2126584ebae
Alejandro <ale2fernandez2003@gmail.com>
2023-12-14 10:37:19 +0100

    nuevo

--- PantallaHoraYFecha.java
+++ PantallaHoraYFecha.java
@@ -7,13 +7,12 @@
 public class PantallaHoraYFecha
 {
     private PantallaDosDigitos pantallaDia;
-
-    private PantallaDosDigitos pantallaMes;
-
-    private PantallaDosDigitos pantallaAño;
-
-    private PantallaDosDigitos pantallaHoras;
     
+    private PantallaDosDigitos pantallaMes;
+    
+    private PantallaDosDigitos pantallaAno;
+    
+    private PantallaDosDigitos pantallaHoras;
     private PantallaDosDigitos pantallaMinutos;
     /**
      * Constructor for objects of class PantallaHoraYFecha
@@ -21,20 +20,40 @@ public class PantallaHoraYFecha
     public PantallaHoraYFecha()
     {
         pantallaDia = new PantallaDosDigitos(1, 31);
-        pantallaMes = new PantallaDosDigitos(1, 12);
-        pantallaAño = new PantallaDosDigitos(1,999999);
+        pantallaMes = new PantallaDosDigitos(1, 13);
+        pantallaAno = new PantallaDosDigitos(1,99);
         pantallaHoras = new PantallaDosDigitos(0, 24);
         pantallaMinutos = new PantallaDosDigitos(0, 60);
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
     public String getFechaYHora()
     {
-        return pantallaHoras.getTextoDeLaPantalla() + ":" + pantallaMinutos.getTextoDeLaPantalla()+" "+pantallaDia.getTextoDeLaPantalla()+"-"+pantallaMes.getTextoDeLaPantalla()+"-"+pantallaAño.getTextoDeLaPantalla();
+        return pantallaHoras.getTextoDeLaPantalla() + ":" + pantallaMinutos.getTextoDeLaPantalla()+" "+pantallaDia.getTextoDeLaPantalla()+"-"+pantallaMes.getTextoDeLaPantalla()+"-"+pantallaAno.getTextoDeLaPantalla();
+    }
+    
+    public void avanzarMinuto (){
+        if (pantallaMinutos.getValorAlmacenado() <= 60) {
+            pantallaMinutos.incrementaValorAlmacenado();
+            if (pantallaMinutos.getValorAlmacenado() == 0) {
+                pantallaHoras.incrementaValorAlmacenado();
+                if (pantallaHoras.getValorAlmacenado() == 0) {
+                    pantallaDia.incrementaValorAlmacenado();
+                    if (pantallaDia.getValorAlmacenado() == 1) {
+                        pantallaMes.incrementaValorAlmacenado();
+                        if (pantallaMes.getValorAlmacenado() == 1) {
+                           pantallaAno.incrementaValorAlmacenado(); 
+                        }
+                    }        
+                }
+            }
+        }
+    }
+    
+    public void fijarFechaYHora (int horas, int minutos, int dia, int mes, int ano){
+        pantallaMinutos.setValorAlmacenado(minutos);
+        pantallaHoras.setValorAlmacenado(horas);
+        pantallaDia.setValorAlmacenado(dia);
+        pantallaMes.setValorAlmacenado(mes);
+        pantallaAno.setValorAlmacenado(ano);
     }
 }
